df1f7ab29e9996eb13763e542ac29a42b1838d82
MFALLAM <mohammed.fathi.allam@gmail.com>
2023-03-21 10:50:20 +0300

    Built BaseActivity as main activity, Built framelayout as container for all activities will leaves inside it as well as a progress bar

--- app/src/main/java/tweakup/ru/retrofitrestapimvvm/RecipeListActivity.java
+++ app/src/main/java/tweakup/ru/retrofitrestapimvvm/RecipeListActivity.java
@@ -1,14 +1,12 @@
 package tweakup.ru.retrofitrestapimvvm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class RecipeListActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recipe_list);
     }
 }
