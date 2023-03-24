6848f687543c87ab5b7049818c9e8c0f36e9de7b
MFALLAM <mohammed.fathi.allam@gmail.com>
2023-03-24 10:43:10 +0300

    Created ServiceGenerator class for Retrofit

---
+++ app/src/main/java/tweakup/ru/retrofitrestapimvvm/requests/ServiceGenerator.java
@@ -0,0 +1,16 @@
+package tweakup.ru.retrofitrestapimvvm.requests;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import tweakup.ru.retrofitrestapimvvm.util.Constants;
+
+public class ServiceGenerator {
+    private static Retrofit.Builder retrofitBuilder =
+            new Retrofit.Builder()
+                    .baseUrl(Constants.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create());
+
+    private static Retrofit retrofit = retrofitBuilder.build();
+
+    
+}
