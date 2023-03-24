021f698732a02e103239d0545624f689d2dd9a99
MFALLAM <mohammed.fathi.allam@gmail.com>
2023-03-24 08:26:41 +0300

    Created Util package and app Constants class for app constants

---
+++ app/src/main/java/tweakup/ru/retrofitrestapimvvm/util/Constants.java
@@ -0,0 +1,8 @@
+package tweakup.ru.retrofitrestapimvvm.util;
+
+public class Constants {
+    
+    public static final String BASE_URL = "https://food2fork.ca/";
+
+    public static final String API_KEY = "Token 9c8b06d329136da358c2d00e76946b0111ce2c48";
+}
